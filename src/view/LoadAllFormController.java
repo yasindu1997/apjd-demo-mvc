31c1d903877cb45ad64f306b0acb3ff2aabce54c
yasindu1997 <yasindusathsara@gmail.com>
2023-02-03 22:15:26 +0530

    table load done

--- src/view/LoadAllFormController.java
+++ src/view/LoadAllFormController.java
@@ -1,10 +1,12 @@
 package view;
 
 import controller.CustomerController;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
 
 import java.net.URL;
@@ -13,7 +15,7 @@ import java.util.ResourceBundle;
 
 public class LoadAllFormController implements Initializable {
     @FXML
-    private TableView<?> tblCustomers;
+    private TableView<Customer> tblCustomers;
 
     @FXML
     private TableColumn<?, ?> colId;
@@ -29,10 +31,18 @@ public class LoadAllFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        //column initialize
+       tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("cid"));
+       tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+       tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("town"));
+       tblCustomers.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("salary"));
+
+       //get customer list from controller
         ArrayList<Customer> customers = CustomerController.loadAllCustomer();
 
-        for(Customer cus : customers){
-            System.out.println(cus.getCid()+" "+cus.getName()+" "+cus.getTown());
-        }
+        //set data to the fx table
+        tblCustomers.setItems(FXCollections.observableArrayList(customers));
+
     }
 }
